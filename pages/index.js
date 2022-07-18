bbe29a07ae08f75071b2f3631cf4654336204d89
Rafael Varela <rafaelvrcr@gmail.com>
2022-07-18 20:31:22 -0300

    Footer call

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../src/components/Header/index';
+import Footer from '../src/components/Footer/index';
 
 
 export default function Home() {
@@ -24,18 +25,7 @@ export default function Home() {
         
       </main>
 
-      <footer >
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
