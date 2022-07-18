b097e1458749415604fea1c46c19de06652216b9
Rafael Varela <rafaelvrcr@gmail.com>
2022-07-18 16:59:46 -0300

    Actvating the use of Global Style

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Title1 = styled.h1`
+color: #FFE742;
+text-align: center;
+text-transform: uppercase;
+`;
+
+const Title2 = styled.h1`
+background-color: #FFE742;
+text-align: center;
+text-transform: uppercase;
+`;
+
+const Title3 = styled.h2`
+text-align: center;
+text-transform: uppercase;
+`;
+
