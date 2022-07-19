74fa6b4faa9b7f7c5a5c41276909b677329ead86
Rafael Varela <rafaelvrcr@gmail.com>
2022-07-18 21:09:00 -0300

    [FIX] Border bottom values

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 import { backgroundColor, terciaryColor } from "../UI/variables";
 
 const StyledHeader = styled.header `
-    background-color: ${backgroundColor};
+    background-color: ${backgroundColor} !important;
     display: flex;
     justify-content: space-between;
     padding: 0 15vw;
     height: 10vh;
     align-items: center;
-    border-bottom: 8px solid var ${terciaryColor};
+    border-bottom: 4px solid ${terciaryColor} !important;
 `;
 
 const StyledA = styled.a `
