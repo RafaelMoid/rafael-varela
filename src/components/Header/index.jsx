b5891b04161239dc8f25b094eb8bfe0f409f6428
Rafael Varela <rafaelvrcr@gmail.com>
2022-07-19 00:34:41 -0300

    Libs added and header refactor start

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,14 +3,57 @@ import styled from 'styled-components';
 import { backgroundColor, terciaryColor } from "../UI/variables";
 
 const StyledHeader = styled.header `
-    background-color: ${backgroundColor} !important;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 15vw;
-    height: 5rem;
-    align-items: center;
-    border-bottom: 4px solid ${terciaryColor} !important;
+height: 55px;
+width: 100%;
+background-color: ${backgroundColor};
+color: #ffffff;
+display: flex;
+align-items: center;
+border-bottom: 4px solid ${terciaryColor} !important;
+position: fixed;
+z-index: 9999;
+> div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0;
+  h2 {
+    font-size: 24px;
+  }
+  ul {
+    display: none;
+    li {
+      a {
+        color: white;
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        transition: 300ms;
+        :hover {
+          color: black;
+          cursor: pointer;
+        }
+      }
+    }
+    @media (min-width: 700px) {
+      display: flex;
+      gap: 20px;
+    }
+  }
+}
 `;
+
+
+//Modelo 1 de Header
+// const StyledHeader = styled.header `
+//     background-color: ${backgroundColor} !important;
+//     display: flex;
+//     justify-content: space-between;
+//     padding: 0 15vw;
+//     height: 5rem;
+//     align-items: center;
+//     border-bottom: 4px solid ${terciaryColor} !important;
+// `;
 
 const StyledA = styled.a `
     color: #ffffff;
