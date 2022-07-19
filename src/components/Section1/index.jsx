4dceea3676554f24bdf2b352a04235c53c11dc63
Rafael Varela <rafaelvrcr@gmail.com>
2022-07-18 21:43:13 -0300

    Creating the section 1

--- src/components/Section1/index.jsx
+++ src/components/Section1/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from 'styled-components';
-import { backgroundColor, terciaryColor } from "../UI/variables";
+import { backgroundColor, primaryColor } from "../UI/variables";
 
 const StyledSection = styled.section `
     background-color: ${backgroundColor} !important;
@@ -9,28 +9,38 @@ const StyledSection = styled.section `
     padding: 5rem 5vw;
     height: 10vh;
     align-items: center;
-    // border-bottom: 4px solid ${terciaryColor} !important;
+    // border-bottom: 4px solid ${primaryColor} !important;
 `;
 
-const StyledFrase = styled.h2 `
-    color: primaryColor;
+const Hero = styled.h1 `
+    color: ${primaryColor};
+    height: 24.375rem;
+    margin: 0 auto;
+    max-height: 54vw;
+    max-width: 43.75rem;
+    position: relative;
+    text-align: center;
+    text-transform: uppercase;
+    width: 90vw;
+    z-index: 1;
+    font-size: 2.25rem;
+    font-weight: 900;
+    line-height: 2.25rem;
 `;
 
-const StyledFrase2 = styled.h2 `
-    background-color: #ffffff;
+const HeroBox = styled.h1 `
+    color: ${backgroundColor};
+    background-color: ${primaryColor};
 `;
-
-const StyledFrase3 = styled.h3 `
-    color: #ffffff;
-`;
-
 
 const Section1 = () => {
     return (
         <StyledSection>
-                <StyledFrase>Rafael</StyledFrase>
-                <StyledFrase2>Varela</StyledFrase2>
-                <StyledFrase3>is awesome</StyledFrase3>
+                <Hero>
+                    <span>Rafael</span>
+                    <HeroBox>Varela</HeroBox>
+                    <span>is awesome!</span>
+                </Hero>
         </StyledSection>
     );
 };
